473391f9803199c9e55e111a1305293336d415ec
Nikhil <longestwavelength@protonmail.com>
2022-09-14 17:46:11 +0530

    NFT minting project

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x774337FC091C2C3dab6fAE0D2ca61155A5d6E0A5";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
